0bbd617396f45d3f76b7b1c374e0ac0e5ffb19e9
Jason Parson <jpsells4u@gmail.com>
2020-06-22 11:38:18 -0500

    created method example

---
+++ OperatorsExample/OperatorsExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OperatorsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The are of a circle with a radius of {r} is {areaOfCircle}");
+
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+
+            Console.WriteLine(k);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
