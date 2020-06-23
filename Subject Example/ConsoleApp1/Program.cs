79564a4da11826bae7d533a6846f7ad6c293f169
Jason Parson <jpsells4u@gmail.com>
2020-06-23 11:22:24 -0500

    created subject exercise

---
+++ Subject Example/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+
+            var userSubject = Console.ReadLine();
+
+
+            switch (userSubject)
+            {
+                case "History":
+                    Console.WriteLine("History is an excellent subject to learn about the past!");
+                    break;
+                case "Math":
+                    Console.WriteLine("Look at you...Math Wizard!");
+                    break;
+                case "Science":
+                    Console.WriteLine("You are the next Albert Einstein!");
+                    break;
+                case "Home Esconomics":
+                    Console.WriteLine("Can you cook dinner tonight?");
+                    break;
+                case "Physical Education":
+                    Console.WriteLine("I'm sure you a an excellent athlete");
+                    break;
+                case "Literature":
+                    Console.WriteLine("Hark! Who goes there?!");
+                    break;
+                default:
+                    Console.WriteLine("Well I guess you don't like school at all");
+                    break;
+            }
+        }
+    }
+}
